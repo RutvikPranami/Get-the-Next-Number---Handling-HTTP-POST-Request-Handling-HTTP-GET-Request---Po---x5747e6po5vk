1bcb7d742a7e14e9b549936f680f0579051482e7
Pranami Rutvik <123811033+RutvikPranami@users.noreply.github.com>
2023-04-26 23:23:36 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const app = express();
 //Middlewares
 app.use(express.json());
 
-// Write a GET Request to get the next number from the input 'num'.
-// Endpoint : /api/get-next-num
-// Return the response as {message : , status: }
+app.get('/api/get-next-num', (req, res) => {
+    res.status(200).json({ "message": req.body.num + 1, "status": "success" })
+})
 
 module.exports = app;
